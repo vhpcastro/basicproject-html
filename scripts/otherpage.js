42973fb0e88f324fd6aa81e38485181379c2998c
Victor Hugo <vhpcastro0811@gmail.com>
2022-08-24 13:49:41 -0300

    Criando pasta scripts, arquivos index.js e otherpage.js

---
+++ scripts/otherpage.js
@@ -0,0 +1,10 @@
+let = document.getElementById('nome')
+let = document.getElementById('recompensa')
+
+function enviar () {
+    alert ("Obrigado " + nome.value + "! O capitão entrará em contato assim que derrotar Kaido!")
+}
+
+function navegar () {
+    alert ("Retornando ao campo de batalha..!")
+}
